a73b1e5b67825a5bd9a2cd651f93c0b141370253
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-05-04 13:59:42 -0400

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express/index.js");
+const morgan = require("morgan/index.js");
+const cors = require("cors/lib/index");
+
 const app = express();
+app.use(cors());
 
 app.use(express.json());
+
+// logging
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+
+morgan.token("data", (request, response) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  } else return;
+});
 
 let persons = [
   {
@@ -26,18 +41,63 @@ let persons = [
   },
 ];
 
-app.get("/info", (req, res) => {
+// Info page
+app.get("/info", (request, response) => {
   date = String(new Date());
-  res.send(
+
+  response.send(
     `<p>Phonebook has ${persons.length} entries.</p>
       <p>${date}</p>`
   );
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+// GET all entries
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
+// Get single entry
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+// Add new entry
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "name or number missing" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+// Run server on port 3001
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ q
@@ -0,0 +1,35 @@
+[33mcommit 2aef9dcb5f4ca31499e12a3b44a672b9db61c51e[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sun May 3 18:05:33 2020 -0400
+
+    Exercise 3.7, 3.8
+
+[33mcommit 1e83d469c2d558862b41029883d1ea512be50671[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sun May 3 17:04:23 2020 -0400
+
+    Exercise 3.3 - 3.6
+
+[33mcommit 9ab957ae2eb60e3317f67f8b51d5b3cfd81ca72f[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sun May 3 16:02:39 2020 -0400
+
+    Exercise 3.2
+
+[33mcommit 1b50700c6126047240203a013ac00b13a68cdfa1[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sun May 3 15:44:50 2020 -0400
+
+    Exercise 3.1
+
+[33mcommit 61d53b5022a9ba12ba53db955118df21cc1b63c0[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sat May 2 21:53:06 2020 -0400
+
+    Created package.json, installed dependencies
+
+[33mcommit 91060cddead5d369245b6943774ea8c9ac463896[m
+Author: Zenghao Wang <zenghao.wang@mail.utoronto.ca>
+Date:   Sat May 2 21:40:24 2020 -0400
+
+    First commit!
